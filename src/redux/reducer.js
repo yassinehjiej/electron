f2ccf87aef06103027250af98761b77c0966283d
yassinehjiej <yassine@yassines-MacBook-Air.local>
2022-10-19 23:22:11 +0100

    install redux, reactRedux and configure it

---
+++ src/redux/reducer.js
@@ -0,0 +1,20 @@
+export default function reducer(state = [], action) {
+    // Here is an example for how reducer works 
+    //
+    //
+    // 
+    // if(action.type === "addCurrentUser") {
+    //     return {
+    //         ...state,
+    //         currentUser: action.payload.currentUser
+    //     }
+    // }
+    // else if(action.type === "changeRoute") {
+    //     return {
+    //         ...state,
+    //         route: action.payload.route
+    //     }
+    // }
+
+    return state;
+}
