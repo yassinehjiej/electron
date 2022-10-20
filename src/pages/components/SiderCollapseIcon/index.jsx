92cbcead0d74d4c22ca0fd655f27fc0db420400b
unknown <sanaa.antari@x-hub.io>
2022-10-20 23:05:57 +0100

    add side menu

---
+++ src/pages/components/SiderCollapseIcon/index.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import Icon from '@ant-design/icons';
+import SiderArrowIcon from '../../../assets/icons/SiderArrowIcon';
+
+export default function SiderCollapseIcon( {rotate} ) {
+  return <Icon component={SiderArrowIcon} rotate={rotate} />;
+}
