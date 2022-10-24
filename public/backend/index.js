8806cf6bc28ae2e1ff3e6a28bed544f10ce66cc1
yassinehjiej <yassine@yassines-MacBook-Air.local>
2022-10-24 15:15:06 +0100

    add advanced search filter

--- public/backend/index.js
+++ public/backend/index.js
@@ -8,6 +8,11 @@ require('./database/db')();
 
 
 app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next()
+  });
 app.use(router);
 app.use(helmet());
 app.use(morgan('tiny'));
--- src/pages/ClientsPage/index.js
+++ src/pages/ClientsPage/index.js
@@ -1,114 +1,144 @@
 import { Button, Input } from "antd";
-import React from "react";
+import React, { useState } from "react";
 import { getIcon } from "../../utils";
 import AddClient from "./components/AddClient";
 import ClientsTable from "./components/ClientsTable";
-import './styles.scss';
+import "./styles.scss";
 
-const data = [{
-    id: '1',
-    name: 'sanaa antari',
-    cin: 'W427330',
-    phone: '0637317786',
-    adress: 'flex justify-start font-sans font-black text-2xl mb-8 flex justify-start font-sans font-black text-2xl mb-8',
-    birthday: '28/01/1999',
-    city: 'Settat',
-},
-{
-    id: '2',
-    name: 'sanaa antari',
-    cin: 'W427330',
-    phone: '0637317786',
-    adress: 'flex justify-start font-sans8',
-    birthday: '28/01/1999',
-    city: 'Settat',
-},
-{
-    id: '3',
-    name: 'sanaa antari',
-    cin: 'W427330',
-    phone: '0637317786',
-    adress: 'flex justify-start font-sans8',
-    birthday: '28/01/1999',
-    city: 'Casablanca',
-},
-{
-    id: '4',
-    name: 'sanaa antari',
-    cin: 'W427330',
-    phone: '0637317786',
-    adress: 'flex justify-start font-sans8',
-    birthday: '28/01/1999',
-    city: 'Casablanca',
-},
-{
-    id: '5',
-    name: 'sanaa antari',
-    cin: 'W427330',
-    phone: '0637317786',
-    adress: 'flex justify-start font-sans8',
-    birthday: '28/01/1999',
-    city: 'Casablanca',
-},
-{
-    id: '6',
-    name: 'sanaa antari',
-    cin: 'W427330',
-    phone: '0637317786',
-    adress: 'flex justify-start font-sans8',
-    birthday: '28/01/1999',
-    city: 'Casablanca',
-},
-{
-    id: '7',
-    name: 'sanaa antari',
-    cin: 'W427330',
-    phone: '0637317786',
-    adress: 'flex justify-start font-sans8',
-    birthday: '28/01/1999',
-    city: 'Casablanca',
-},
-{
-    id: '8',
-    name: 'sanaa antari',
-    cin: 'W427330',
-    phone: '0637317786',
-    adress: 'flex justify-start font-sans8',
-    birthday: '28/01/1999',
-    city: 'Casablanca',
-},
-{
-    id: '9',
-    name: 'sanaa antari',
-    cin: 'W427330',
-    phone: '0637317786',
-    adress: 'flex justify-start font-sans8',
-    birthday: '28/01/1999',
-    city: 'Casablanca',
-},
+export const data = [
+  {
+    id: "1",
+    name: "sanaa antari",
+    cin: "W427330",
+    phone: "0637317786",
+    adress:
+      "flex justify-start font-sans font-black text-2xl mb-8 flex justify-start font-sans font-black text-2xl mb-8",
+    birthday: "1999-11-30T00:00:00.000Z",
+    lastVisit: "1999-11-30T00:00:00.000Z",
+    city: "Settat",
+  },
+  {
+    id: "2",
+    name: "sanaa antari",
+    cin: "W427330",
+    phone: "0637317786",
+    adress: "flex justify-start font-sans8",
+    birthday: "1999-11-30T00:00:00.000Z",
+    lastVisit: "1999-11-30T00:00:00.000Z",
+    city: "Settat",
+  },
+  {
+    id: "3",
+    name: "sanaa antari",
+    cin: "W427330",
+    phone: "0637317786",
+    adress: "flex justify-start font-sans8",
+    birthday: "1999-11-30T00:00:00.000Z",
+    lastVisit: "1999-11-30T00:00:00.000Z",
+    city: "Casablanca",
+  },
+  {
+    id: "4",
+    name: "sanaa antari",
+    cin: "W427330",
+    phone: "0637317786",
+    adress: "flex justify-start font-sans8",
+    birthday: "1999-11-30T00:00:00.000Z",
+    lastVisit: "1999-11-30T00:00:00.000Z",
+    city: "Casablanca",
+  },
+  {
+    id: "5",
+    name: "sanaa antari",
+    cin: "W427330",
+    phone: "0637317786",
+    adress: "flex justify-start font-sans8",
+    lastVisit: "1999-11-30T00:00:00.000Z",
+    birthday: "1999-11-30T00:00:00.000Z",
+    city: "Casablanca",
+  },
+  {
+    id: "6",
+    name: "sanaa antari",
+    cin: "W427330",
+    phone: "0637317786",
+    adress: "flex justify-start font-sans8",
+    lastVisit: "1999-11-30T00:00:00.000Z",
+    birthday: "1999-11-30T00:00:00.000Z",
+    city: "Casablanca",
+  },
+  {
+    id: "7",
+    name: "sanaa antari",
+    cin: "W427330",
+    phone: "0637317786",
+    adress: "flex justify-start font-sans8",
+    lastVisit: "1999-11-30T00:00:00.000Z",
+    birthday: "1999-11-30T00:00:00.000Z",
+    city: "Casablanca",
+  },
+  {
+    id: "8",
+    name: "sanaa antari",
+    cin: "W427392",
+    phone: "0637317786",
+    adress: "flex justify-start font-sans8",
+    lastVisit: "1999-11-30T00:00:00.000Z",
+    birthday: "1999-11-30T00:00:00.000Z",
+    city: "Casablanca",
+  },
+  {
+    id: "9",
+    name: "yassine",
+    cin: "UA5666",
+    gender: "male",
+    phone: "0643545792",
+    adress: "flex justify-start font-sans8",
+    lastVisit: "1999-12-30T00:00:00.000Z",
+    birthday: "1999-11-30T00:00:00.000Z",
+    city: "Casablanca",
+  },
 ];
 
 export default function ClientsPage() {
-    const refreshIcon = getIcon('refresh');
-    return(
-        <div className="w-full p-8"> 
-            <div className="flex justify-start font-sans font-bold text-2xl mb-8">
-                Liste des clients
-            </div>
-            <div className="flex justify-between">
-                <Input
-                size="large"
-                placeholder=' Search'
-                className="search-input"
-                />
-                <div className="flex">
-                    <Button className="btn-refresh">
-                        <refreshIcon.icon />
-                    </Button>
-                    <AddClient />
-                </div>
-            </div>
-           <ClientsTable data={data} />
+  const [d, updateD] = useState(data);
+
+  const onChange = (value) => {
+    const result = [
+      data.filter(
+        (element) =>
+          element.name.toLowerCase().includes(value.toLowerCase()) ||
+          element.cin.toLowerCase().includes(value.toLowerCase()) ||
+          element.phone.toLowerCase().includes(value.toLowerCase()) ||
+          element.birthday.toLowerCase().includes(value.toLowerCase()) ||
+          element.city.toLowerCase().includes(value.toLowerCase()) ||
+          element.lastVisit.toLowerCase().includes(value.toLowerCase())
+      ),
+    ];
+    updateD(result[0]);
+  };
+
+  const refreshIcon = getIcon("refresh");
+  return (
+    <div className="w-full p-8">
+      <div className="flex justify-start font-sans font-bold text-2xl mb-8">
+        Liste des clients
+      </div>
+      <div className="flex justify-between">
+        <Input
+          size="large"
+          placeholder=" Search"
+          className="search-input"
+          onChange={(event) => onChange(event.target.value)}
+        />
+        <div className="flex">
+          <Button className="btn-refresh">
+            <refreshIcon.icon />
+          </Button>
+          <AddClient />
         </div>
-    );
+      </div>
+      <ClientsTable data={d} />
+    </div>
+  );
 }
